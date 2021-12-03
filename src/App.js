a6d45ee0db0b3b238498233f81245aafaca36bd5
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-12-03 23:27:46 +0700

    task

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Task1 } from './Tasks/Task1';
-import { Task2 } from './Tasks/Task2';
+// import { Task1 } from './Tasks/Task1';
 import { data } from './Task1data';
 import './index.css';
+import { Task2 } from './Tasks/Task2';
 
 function App() {
     return (
         <div className="App">
-            <Task1 array={data} />
-            <Task2 />
+            {/* <Task1 /> */}
+            <Task2 array={data} />
         </div>
     );
 }
--- src/Tasks/Task2.js
+++ src/Tasks/Task2.js
@@ -1,9 +1,25 @@
-import React from 'react';
+// const array = [607, 618, 618, 617, 647, 716, 769, 792];
 
-export const Task2 = () => {
+export const Task2 = ({ array }) => {
+    let counter = 0;
+    for (let i = 0; i < array.length; i += 1) {
+        const el = array[i];
+        const next1 = array[i + 1];
+        const next2 = array[i + 2];
+        const next3 = array[i + 3];
+
+        if ((el + next1 + next2) / 3 < (next1 + next2 + next3) / 3) {
+            console.log('vot ono');
+            counter += 1;
+        }
+    }
     return (
         <div>
-            <h2>Task2</h2>
+            <h2>Task1</h2>
+            <p>
+                How many measurements are larger than the previous measurement?
+            </p>
+            <p> {counter}</p>
             <hr />
         </div>
     );
